6e9d46d78c22fc715fe7c97740a62fc322a8898b
Deepak Kushwaha <deepak2003kushwaha@gmail.com>
2025-04-04 22:51:59 +0530

    r2 project v2

--- routes/project.routes.js
+++ routes/project.routes.js
@@ -24,3 +24,18 @@ router.put('/add-user',
         .custom((users) => users.every(user => typeof user === 'string')).withMessage('Each user must be a string'),
     projectController.addUserToProject
 )
+
+router.get('/get-project/:projectId',
+    authMiddleWare.authUser,
+    projectController.getProjectById
+)
+
+router.put('/update-file-tree',
+    authMiddleWare.authUser,
+    body('projectId').isString().withMessage('Project ID is required'),
+    body('fileTree').isObject().withMessage('File tree is required'),
+    projectController.updateFileTree
+)
+
+
+export default router;
