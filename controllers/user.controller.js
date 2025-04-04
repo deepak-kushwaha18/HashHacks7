59c256c1ca27da31860e9ff8724b3420fb65e2d7
Deepak Kushwaha <deepak2003kushwaha@gmail.com>
2025-04-04 23:17:48 +0530

    added user profile,logout controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -65,3 +65,49 @@ export const loginController = async (req, res) => {
         res.status(400).send(err.message);
     }
 }
+
+export const profileController = async (req, res) => {
+
+    res.status(200).json({
+        user: req.user
+    });
+
+}
+
+export const logoutController = async (req, res) => {
+    try {
+
+        const token = req.cookies.token || req.headers.authorization.split(' ')[ 1 ];
+
+        redisClient.set(token, 'logout', 'EX', 60 * 60 * 24);
+
+        res.status(200).json({
+            message: 'Logged out successfully'
+        });
+
+
+    } catch (err) {
+        console.log(err);
+        res.status(400).send(err.message);
+    }
+}
+
+export const getAllUsersController = async (req, res) => {
+    try {
+
+        const loggedInUser = await userModel.findOne({
+            email: req.user.email
+        })
+
+        const allUsers = await userService.getAllUsers({ userId: loggedInUser._id });
+
+        return res.status(200).json({
+            users: allUsers
+        })
+
+    } catch (err) {
+
+        console.log(err)
+
+    }
+}
