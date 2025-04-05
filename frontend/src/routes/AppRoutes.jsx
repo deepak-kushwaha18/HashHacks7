88ad1f40d7a5366e1eb02e3856835428bf01847b
Deepak Kushwaha <deepak2003kushwaha@gmail.com>
2025-04-05 05:40:35 +0530

    new fixed file

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -5,6 +5,7 @@ import Register from '../screens/Register'
 import Home from '../screens/Home'
 import Project from '../screens/Project'
 import UserAuth from '../auth/UserAuth'
+import First from "../screens/First"
 
 const AppRoutes = () => {
     return (
@@ -15,6 +16,7 @@ const AppRoutes = () => {
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/project" element={<UserAuth><Project /></UserAuth>} />
+                <Route path="/first" element={<First/>}></Route>
             </Routes>
 
         </BrowserRouter>
